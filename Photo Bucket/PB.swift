039b7bd07878bae7470fef5bb4d7c2a1b0f043e1
rhit-yinz1 <yinz1@rose-hulman.edu>
2021-08-08 17:04:35 -0400

    pb

---
+++ Photo Bucket/PB.swift
@@ -0,0 +1,18 @@
+//
+//  PB.swift
+//  Photo Bucket
+//
+//  Created by Theo Yin on 8/8/21.
+//
+
+import Foundation
+
+class PB {
+    var url: String
+    var caption: String
+    
+    init(url: String, caption: String){
+        self.url = url
+        self.caption = caption
+    }
+}
--- Photo Bucket/PBViewController.swift
+++ Photo Bucket/PBViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 
 class PBViewController: UITableViewController {
     let pbCellIdentifier = "PBCellIdentifier"
-    var names = ["a", "b", "c", "d"]
+    var pbs = [PB]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pbs.append(PB(url: "urrrl1", caption: "things1"))
+        pbs.append(PB(url: "urrrl2", caption: "things2"))
+
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return pbs.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: pbCellIdentifier, for: indexPath)
         //configure cell
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = pbs[indexPath.row].caption
         return cell
     }
 }
